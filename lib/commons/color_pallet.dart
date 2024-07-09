e0f9fa2bf53f994732d8eaa4c1520270734970b8
jeon-jueun <dudwns0423@naver.com>
2024-07-09 16:17:18 +0900

    /#1/OAuth 로그인 화면 틀 + 컬러팔레트

---
+++ lib/commons/color_pallet.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'dart:core';
+
+class ColorPallet {
+  Color gray_d9 = const Color(0xFFD9D9D9);
+  Color gray_82 = const Color(0xFF828282);
+  Color beige = const Color(0xFFFAEBD7);
+  Color deep_green = const Color(0xFF2F4F4F);
+  Color light_green = const Color(0xFF96B8B8);
+}
