e91c640f9b12eeddd9be11530d7477a06ffe683a
sujeong <qkrtnwjd4212@naver.com>
2024-07-15 21:43:26 +0900

    feat/#9/네이버 지도 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
-import 'package:docent/screen/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:docent/screen/HomePage.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'OAuth/login_UI.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load();
-  await NaverMapSdk.instance.initialize(clientId: dotenv.env['NAVER_MAP_CLIENT_ID']);
+  await dotenv.load(fileName: '.env');
+  await NaverMapSdk.instance.initialize(
+      clientId: dotenv.env['NAVER_MAP_CLIENT_ID']!,
+      onAuthFailed: (ex) {
+        print("********* 네이버맵 인증오류 : $ex *********");
+      });
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/screen/HeritagePage.dart
+++ lib/screen/HeritagePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 import '../commons/BottomBar.dart';
 
@@ -23,7 +24,19 @@ class _HeritagePageState extends State<HeritagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: NaverMap(
+              options: NaverMapViewOptions(),
+              onMapReady: (controller) {
+                print('네이버 맵 로딩됨!');
+              },
+            ),
+          ),
 
+        ],
+      ),
       bottomNavigationBar: BottomBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
