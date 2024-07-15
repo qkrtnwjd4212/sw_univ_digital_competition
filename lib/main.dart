e10277cd29ca188fe001b511a6cfbc3ebfbbe0ba
sujeong <qkrtnwjd4212@naver.com>
2024-07-15 14:39:08 +0900

    feat/#2/Naver map 패키지 연결 및 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:docent/screen/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'OAuth/login_UI.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+  await NaverMapSdk.instance.initialize(clientId: dotenv.env['NAVER_MAP_CLIENT_ID']);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
