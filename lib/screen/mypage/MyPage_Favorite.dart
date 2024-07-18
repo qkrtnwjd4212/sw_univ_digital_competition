010da9317c8ecb5370d2b741cadb6b4ac391b276
sujeong <qkrtnwjd4212@naver.com>
2024-07-19 00:59:47 +0900

    feat/#18/회원정보 페이지, 좋아요한 문화재 페이지 UI 작업

--- lib/screen/mypage/MyPage_Favorite.dart
+++ lib/screen/mypage/MyPage_Favorite.dart
@@ -1,3 +1,4 @@
+import 'package:docent/commons/HeritageBox.dart';
 import 'package:flutter/material.dart';
 import '../../commons/BottomBar.dart';
 import '../../commons/color_pallet.dart';
@@ -42,7 +43,259 @@ class _MypageFavoriteState extends State<MypageFavorite> {
           width: width * 0.9,
           child: Column(
             children: [
+              SizedBox(height: height * 0.01,),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: width * 0.25,
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Image.network(
+                                  'https://img.khan.co.kr/news/2015/03/24/l_2015032501003866400310141.jpg',
+                                  fit: BoxFit.cover,
+                                  width: width * 0.25,
+                                  height: 90,
+                                )
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // 좋아요 로직 추가
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(5),
+                                    child: Icon(
+                                      Icons.favorite_rounded,
+                                      color: Colors.red,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '경복궁 경회루',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    size: 17,
+                                    color: colorPallet.light_green,
+                                  ),
+                                  SizedBox(width: 5), // 아이콘과 텍스트 사이에 약간의 간격 추가
+                                  Expanded(
+                                    child: Text(
+                                      '서울특별시 종로구 사직로 161 (세종로, 경복궁)',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Color(0xffC3C3C3),
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xffC3C3C3),
+                      thickness: 1,
+                      height: 20,
+                    ),
 
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: width * 0.25,
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image.network(
+                                    'https://i.namu.wiki/i/zpqQTS8IhybFCtNnOOXhIt8E0f2Bfr4qtyKCv5nTXaGkp8kSsIQBxq4-oJWyqUYAf3gYaLkr6ayUmMClXvBMWA.webp',
+                                    fit: BoxFit.cover,
+                                    width: width * 0.25,
+                                    height: 90,
+                                  )
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // 좋아요 로직 추가
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(5),
+                                    child: Icon(
+                                      Icons.favorite_rounded,
+                                      color: Colors.red,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '합천 해인사 대장경판',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    size: 17,
+                                    color: colorPallet.light_green,
+                                  ),
+                                  SizedBox(width: 5), // 아이콘과 텍스트 사이에 약간의 간격 추가
+                                  Expanded(
+                                    child: Text(
+                                      '경남 합천군 가야면 해인사길 122, 해인사 (치인리)',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Color(0xffC3C3C3),
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xffC3C3C3),
+                      thickness: 1,
+                      height: 20,
+                    ),
+
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: width * 0.25,
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image.network(
+                                    'https://www.yeongnam.com/mnt/file/201204/20120402.010310721200001i1.jpg',
+                                    fit: BoxFit.cover,
+                                    width: width * 0.25,
+                                    height: 90,
+                                  )
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // 좋아요 로직 추가
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(5),
+                                    child: Icon(
+                                      Icons.favorite_rounded,
+                                      color: Colors.red,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '청자 기린형뚜껑 향로',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    size: 17,
+                                    color: colorPallet.light_green,
+                                  ),
+                                  SizedBox(width: 5), // 아이콘과 텍스트 사이에 약간의 간격 추가
+                                  Expanded(
+                                    child: Text(
+                                      '서울특별시 성북구 성북로 102-11',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Color(0xffC3C3C3),
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xffC3C3C3),
+                      thickness: 1,
+                      height: 20,
+                    ),
+
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- lib/screen/mypage/MyPage_MemberInfo.dart
+++ lib/screen/mypage/MyPage_MemberInfo.dart
@@ -55,17 +55,24 @@ class _MyPageMemberInfoState extends State<MyPageMemberInfo> {
                     ),
                   ),
                   SizedBox(width: 20,),
-                  Container(
-                    color: Color(0xffF4F4F4), // 연한 회색 배경색 설정
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: '전주은', // 힌트 텍스트 설정
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black, // 텍스트 색상 설정
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffF4F4F4),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.only(left: 10),
+                      child: TextFormField(
+                        cursorColor: colorPallet.deep_green,
+                        decoration: InputDecoration(
+                          hintText: '전주은', // 힌트 텍스트 설정
+                          hintStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
+                          border: InputBorder.none, // 외곽선 없애기
                         ),
-                        border: InputBorder.none, // 외곽선 없애기
                       ),
                     ),
                   )
@@ -79,7 +86,7 @@ class _MyPageMemberInfoState extends State<MyPageMemberInfo> {
                 thickness: 1,
                 height: 10,
               ),
-
+              SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -88,7 +95,7 @@ class _MyPageMemberInfoState extends State<MyPageMemberInfo> {
                       fontWeight: FontWeight.w500
                   ),),
                   SizedBox(
-                    width: 75,
+                    width: 70,
                     height: 35,
                     child: TextButton(onPressed: () {
 
@@ -97,6 +104,7 @@ class _MyPageMemberInfoState extends State<MyPageMemberInfo> {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
+                      padding: EdgeInsets.fromLTRB(5, 0, 5, 0)
                     ),
                         child: Text(
                           '추가', style: TextStyle(
@@ -107,7 +115,169 @@ class _MyPageMemberInfoState extends State<MyPageMemberInfo> {
                   )
                 ],
               ),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 70,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {
+                        // 버튼이 눌렸을 때의 동작
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: colorPallet.light_green, // 버튼 배경색 설정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5), // 모서리를 5만큼 둥글게
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 8), // 내부 패딩 조정
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '궁궐',
+                            style: TextStyle(
+                              color: Colors.white, // 텍스트 색상 설정
+                              fontSize: 16,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
 
+                            },
+                            child: Icon(
+                              Icons.close,
+                              color: colorPallet.deep_green,
+                              size: 16
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+
+                  SizedBox(
+                    width: 70,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {
+                        // 버튼이 눌렸을 때의 동작
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: colorPallet.light_green, // 버튼 배경색 설정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5), // 모서리를 5만큼 둥글게
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 8), // 내부 패딩 조정
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '청자',
+                            style: TextStyle(
+                              color: Colors.white, // 텍스트 색상 설정
+                              fontSize: 16,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+
+                            },
+                            child: Icon(
+                                Icons.close,
+                                color: colorPallet.deep_green,
+                                size: 16
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+
+                  SizedBox(
+                    width: 70,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {
+                        // 버튼이 눌렸을 때의 동작
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: colorPallet.light_green, // 버튼 배경색 설정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5), // 모서리를 5만큼 둥글게
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 8), // 내부 패딩 조정
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '석탑',
+                            style: TextStyle(
+                              color: Colors.white, // 텍스트 색상 설정
+                              fontSize: 16,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+
+                            },
+                            child: Icon(
+                                Icons.close,
+                                color: colorPallet.deep_green,
+                                size: 16
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+
+                  SizedBox(
+                    width: 70,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {
+                        // 버튼이 눌렸을 때의 동작
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: colorPallet.light_green, // 버튼 배경색 설정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5), // 모서리를 5만큼 둥글게
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 8), // 내부 패딩 조정
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '불전',
+                            style: TextStyle(
+                              color: Colors.white, // 텍스트 색상 설정
+                              fontSize: 16,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+
+                            },
+                            child: Icon(
+                                Icons.close,
+                                color: colorPallet.deep_green,
+                                size: 16
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
 
 
             ],
