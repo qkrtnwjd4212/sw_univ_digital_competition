dce27a74bab8e353b023b4623b0f133d8b8583c1
sujeong <qkrtnwjd4212@naver.com>
2024-07-18 15:48:00 +0900

    feat/#9/문화재 상세 페이지 UI 작업, 데이터 연결

--- lib/commons/HeritageBox.dart
+++ lib/commons/HeritageBox.dart
@@ -1,3 +1,4 @@
+import 'package:docent/screen/Heritage/HeritageDetail.dart';
 import 'package:flutter/material.dart';
 import '../commons/color_pallet.dart';
 import 'package:get/get.dart';
@@ -26,112 +27,117 @@ class HeritageBox extends StatelessWidget {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
-    return FutureBuilder<InfoDetail>(
-      future: InfoDetailService.fetchInfoDetail(info),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator(
-            color: colorPallet.light_green,
-          ));
-        }
-        if (snapshot.hasError) {
-          return Center(child: Text('Error loading details'));
-        }
-        final detail = snapshot.data!;
-        return Container(
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: width * 0.25,
-                    child: Stack(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: img != null
-                              ? Image.network(
-                            img!,
-                            fit: BoxFit.cover,
-                            width: width * 0.25,
-                            height: 80,
-                          )
-                              : Container(
-                            width: width * 0.25,
-                            height: 80,
-                            color: Colors.grey[300],
-                          ),
-                        ),
-                        Positioned(
-                          top: 0,
-                          right: 0,
-                          child: GestureDetector(
-                            onTap: () {
-                              // 좋아요 로직 추가
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              child: Icon(
-                                Icons.favorite_border_outlined,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 15),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          name,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                        SizedBox(height: 5),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on,
-                              size: 17,
-                              color: colorPallet.light_green,
-                            ),
-                            SizedBox(width: 5), // 아이콘과 텍스트 사이에 약간의 간격 추가
-                            Expanded(
-                              child: Text(
-                                detail.ccbaLcad,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color(0xffC3C3C3),
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Divider(
-                color: Color(0xffC3C3C3),
-                thickness: 1,
-                height: 20,
-              ),
-            ],
-          ),
-        );
+    return GestureDetector(
+      onTap: () {
+        Get.to(() => HeritageDetail(info: info));
       },
+      child: FutureBuilder<InfoDetail>(
+        future: InfoDetailService.fetchInfoDetail(info),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(
+              color: colorPallet.light_green,
+            ));
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error loading details'));
+          }
+          final detail = snapshot.data!;
+          return Container(
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: width * 0.25,
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: img != null
+                                ? Image.network(
+                              img!,
+                              fit: BoxFit.cover,
+                              width: width * 0.25,
+                              height: 80,
+                            )
+                                : Container(
+                              width: width * 0.25,
+                              height: 80,
+                              color: Colors.grey[300],
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: GestureDetector(
+                              onTap: () {
+                                // 좋아요 로직 추가
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                child: Icon(
+                                  Icons.favorite_border_outlined,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                size: 17,
+                                color: colorPallet.light_green,
+                              ),
+                              SizedBox(width: 5), // 아이콘과 텍스트 사이에 약간의 간격 추가
+                              Expanded(
+                                child: Text(
+                                  detail.ccbaLcad,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color(0xffC3C3C3),
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Color(0xffC3C3C3),
+                  thickness: 1,
+                  height: 20,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: HeritageListPage(),
+      home: HomePage(),
       title: 'Flutter Demo',
     );
   }
